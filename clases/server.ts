71f3f87939cde087e43617cd6323392b6f964dd0
David Dorantes <j.v.dorantes@evomatik.com>
2019-09-09 18:00:15 -0500

    Se agregan configuraciones de sockets

--- clases/server.ts
+++ clases/server.ts
@@ -1,15 +1,29 @@
 import express from 'express';
 import { SERVER_PORT } from '../global/enviroment';
-
+import socketIO from 'socket.io';
+import http from 'http';
 export default class Server {
     public app: express.Application;
     public port: number;
-
+    public io:SocketIO.Server;
+    public httpServer: http.Server;
     constructor(){
         this.app = express();
         this.port = SERVER_PORT;
+        this.httpServer = new http.Server(this.app);
+        this.io = socketIO(this.httpServer);
+
+        this.escuchaSocket();
     }
+
+    private escuchaSocket(){
+        console.log('Escuchando conexiones - Sockets');
+        this.io.on('connection',cliente =>{
+            console.log('Nuevo cliente');
+        });
+    }
+
     start (callback: Function){
-        this.app.listen(this.port,callback);
+        this.httpServer.listen(this.port,callback);
     }
 }
